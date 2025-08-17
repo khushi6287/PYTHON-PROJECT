51416f67f191707f3b74d44dcd2325435615ca10
KHUSHI NAGAR <khushinagar6287@gmail.com>
2025-08-17 23:42:05 +0530

    Add files via upload

---
+++ Project-2 Logic Box.py
@@ -0,0 +1,38 @@
+print("Welcome to the Pattern Generator and Number Analyzer!")
+
+while True:
+    print("\nSelect an option:")
+    print("1.Generate a Pattern")
+    print("2.Analyze a Range of Numbers")
+    print("3.Exit")
+
+    choice = input("Enter your choice: ")
+
+    if choice == "1":
+        rows = int(input("Enter the number of row for the pattern:  "))
+        print("Pattern:")
+        for i in range(1, rows + 1):
+            print("*" * i)
+
+    elif choice == "2":
+        start = int(input("Enter the start of the range: "))
+        end = int(input("Enter the end of the range: "))
+
+        total = 0
+        for num in range(start, end + 1):
+            if num % 2 == 0:
+                print(f"Number {num} is Even")
+            else:
+                print(f"Number {num} is Odd")
+            
+            total += num
+
+        print(f"Sum of all number from {start} to {end} is: {total}")
+
+    elif choice == "3":
+        print("Goodbye!")
+        break
+
+    else:
+        print("Invalid choice. Please enter 1, 2, or 3.")
+
